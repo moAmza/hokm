778b1fc4265ed43687fbb0b1e078c6e2cc3190ab
moAmza <mohamad.zangooei@outlook.com>
2024-02-04 12:30:41 +0330

    apply format to all files

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express'
+import dotenv from 'dotenv'
 
 const run = async () => {
-  dotenv.config();
-  const app = express();
+  dotenv.config()
+  const app = express()
 
-  app.use((req, res) => res.send("Hello"));
+  app.use((req, res) => res.send('Hello'))
   app.listen(process.env.PORT, () =>
-    console.log(`listenning on port ${process.env.PORT}`)
-  );
-};
+    console.log(`listenning on port ${process.env.PORT}`),
+  )
+}
 
-run();
+run()
